748a033bb49923feeb4f1b221ea649ad9551246f
Dustin Sallings <dustin@spy.net>
2008-02-29 15:17:03 -0800

    Initial version of the tree.
    I basically have the cocoa and twisted interfaces not pissed at
    each other.

---
+++ main.py
@@ -0,0 +1,20 @@
+#
+#  main.py
+#  BuildWatch
+#
+#  Created by Dustin Sallings on 2/28/08.
+#  Copyright __MyCompanyName__ 2008. All rights reserved.
+#
+
+#import modules required by application
+import objc
+import Foundation
+import AppKit
+
+from PyObjCTools import AppHelper
+
+# import modules containing classes required to start application and load MainMenu.nib
+import BuildWatchAppDelegate
+
+# pass control to AppKit
+AppHelper.runEventLoop()
